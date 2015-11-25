effa7b1ea7a65aab927ea1fadca6798c96408679
Tiago Royer <royertiago@gmail.com>
2015-11-25 19:49:44 -0200

    ballistics.cpp: Finalize constructor's implementation.

--- ballistics.cpp
+++ ballistics.cpp
@@ -1,41 +1,23 @@
 #include <cmath>
+#include "math.hpp"
 #include "ballistics.hpp"
 
-Ballistics::Ballistics(cv::Mat p1, cv::Mat p2, double alpha, cv::Mat q) {
-    _front = p2 - p1;
+Ballistics::Ballistics(cv::Mat p1, cv::Mat p2, double alpha, cv::Mat q1, cv::Mat q2) {
+    _center = line_line_intersection(p1, p2, q1, q2);
 
-    /* Find the intersection between the line p1-p2 and the line l,
-     * that passes through q and intersects p1-p2 with an angle of alpha.
-     * First, shift the world so that p1-p2 is on the origin.
+    /* old_front is the orthonormal vector
+     * that pointed to the front view of the laser,
+     * in the first two measurements.
      */
-    cv::Mat qq = q - p1;
-    // Now, project qq on the line p1-p2.
-    cv::Mat pqq = qq.dot(_front) * _front;
+    cv::Mat old_front = p2 - p1;
+    old_front /= cv::norm(old_front);
 
-    /* We now have a triangle, formed by the points _center, qq and pqq.
-     * The side c-pqq lies on the line spanned by _front.
-     * Due to the projection, this triangle is right on pqq
-     * and we have the size of pqq-qq, so using trigonomotry
-     * we can compute the distance c-pqq, thus obtaining c.
-     */
-    double oppositeCathetus = cv::norm(pqq-qq);
-    double adjacentCathetus = oppositeCathetus / std::atan(alpha);
-    _center = pqq - adjacentCathetus * _front;
-    // Finally, shift back
-    _center += p1;
+    _front = q2 - q1;
+    _front /= cv::norm(_front);
 
-    /* To compute the main rotation axis,
-     * which is orthogonal both to the former _front
-     * and the current _front (which is q - _center),
-     * we will compute the cross product between these two vectors.
-     */
-    _up = _front.cross(q - _center);
-    // Now, reposition the _front vector to the current front.
-    _front = q - _center;
+    double actual_angle = std::acos(old_front.dot(_front));
+    double projection_angle = angle_of_projection( actual_angle, alpha );
 
-    /* Both rotation axis are orthogonal,
-     * and the secondary axis is orthogonal to _front;
-     * thus, another cross-product gives us _left.
-     */
+    _up = compute_rotation_axis( old_front, _front, projection_angle );
     _left = _up.cross(_front);
 }
