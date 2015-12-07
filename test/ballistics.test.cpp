c603653a6468d1584bb1210897eb4490d1b491c8
Tiago Royer <royertiago@gmail.com>
2015-12-07 18:36:52 -0200

    test/ballistics.test.cpp: Add construction test.

--- test/ballistics.test.cpp
+++ test/ballistics.test.cpp
@@ -21,3 +21,17 @@ cv::Mat vec( double a, double b, double c ) {
 TEST_CASE( "assert_near self-test" ) {
     assert_near( vec(0, 0, 0), vec(0, 0, 1e-7), 1e-5 );
 }
+
+TEST_CASE( "Construction of an origin-centered Ballistics object", "[ballistics]" ) {
+    Ballistics eniac(
+            (cv::Mat_<double>(3, 1) << std::sqrt(2)/2, std::sqrt(2)/2, 1),
+            (cv::Mat_<double>(3, 1) << std::sqrt(2), std::sqrt(2), 2),
+            3.14159/4,
+            (cv::Mat_<double>(3, 1) << 1, 0, 1),
+            (cv::Mat_<double>(3, 1) << 2, 0, 2)
+        );
+    assert_near( eniac.center(), vec( 0, 0, 0 ) );
+    assert_near( eniac.up(), vec( 0, 0, 1 ) );
+    assert_near( eniac.front(), vec( std::sqrt(2)/2, 0, std::sqrt(2)/2) );
+    assert_near( eniac.left(), vec( 0, 1, 0 ) );
+}
