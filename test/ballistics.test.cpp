692e17c42c21f2cc6ba6d4c66d3c1892d35050be
Tiago Royer <royertiago@gmail.com>
2015-12-07 21:27:37 -0200

    test/ballistics.test.cpp: Improve line error reporting.

--- test/ballistics.test.cpp
+++ test/ballistics.test.cpp
@@ -9,10 +9,12 @@
  * because it logs the matrices values before running the test,
  * thus producing better error messages.
  */
-void assert_near( cv::Mat a, cv::Mat b,
+void assert_near( int line, cv::Mat a, cv::Mat b,
     double epsilon = std::numeric_limits<double>::epsilon() * 100
 ) {
-    INFO( a << "\n !=~ \n" << b << ", epsilon = " << epsilon );
+    INFO( "line = " << line );
+    INFO( "epsilon = " << epsilon );
+    INFO( a << "\n !=~ \n" << b );
     CHECK( cv::norm(a - b) < epsilon );
 }
 
@@ -21,8 +23,11 @@ cv::Mat vec( double a, double b, double c ) {
     return cv::Mat_<double>(3, 1) << a, b, c;
 }
 
+// More syntatic sugar
+#define ASSERT_NEAR( ... ) assert_near( __LINE__, __VA_ARGS__ )
+
 TEST_CASE( "assert_near self-test" ) {
-    assert_near( vec(0, 0, 0), vec(0, 0, 1e-7), 1e-5 );
+    ASSERT_NEAR( vec(0, 0, 0), vec(0, 0, 1e-7), 1e-5 );
 }
 
 TEST_CASE( "Construction of an origin-centered Ballistics object", "[ballistics]" ) {
@@ -32,22 +37,22 @@ TEST_CASE( "Construction of an origin-centered Ballistics object", "[ballistics]
     cv::Mat q2 = vec(2, 0, 2);
     double angle = 3.141592653589793238462643383/4;
 
-    auto check = []( Ballistics & eniac ) {
-        assert_near( eniac.center(), vec( 0, 0, 0 ) );
-        assert_near( eniac.up(), vec( 0, 0, 1 ) );
-        assert_near( eniac.front(), vec( std::sqrt(2)/2, 0, std::sqrt(2)/2) );
-        assert_near( eniac.left(), vec( 0, 1, 0 ) );
+    auto check = []( Ballistics & eniac, int line ) {
+        assert_near( line, eniac.center(), vec( 0, 0, 0 ) );
+        assert_near( line, eniac.up(), vec( 0, 0, 1 ) );
+        assert_near( line, eniac.front(), vec( std::sqrt(2)/2, 0, std::sqrt(2)/2) );
+        assert_near( line, eniac.left(), vec( 0, 1, 0 ) );
     };
 
     Ballistics eniac( p1, p2, angle, q1, q2 );
-    check( eniac );
+    check( eniac, __LINE__ );
 
     eniac = Ballistics( p2, p1, angle, q1, q2 );
-    check( eniac );
+    check( eniac, __LINE__ );
 
     eniac = Ballistics( p1, p2, angle, q2, q1 );
-    check( eniac );
+    check( eniac, __LINE__ );
 
     eniac = Ballistics( p2, p1, angle, q2, q1 );
-    check( eniac );
+    check( eniac, __LINE__ );
 }
