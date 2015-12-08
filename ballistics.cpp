736bbc1cf9f7f5f981724a2423c6c50be19fe402
Tiago Royer <royertiago@gmail.com>
2015-12-08 12:09:24 -0200

    ballistics.cpp: Fix range error on Ballistics::align.

--- ballistics.cpp
+++ ballistics.cpp
@@ -71,7 +71,23 @@ Ballistics::rotation_data Ballistics::align(cv::Mat p) {
         ret.main = 0.0;
     }
     else {
-        ret.main = std::acos( pp.dot(_front) / cv::norm(pp) );
+        fp /= cv::norm(fp);
+        pp /= cv::norm(pp);
+        double dot_product = pp.dot(fp);
+        /* Mathematically, the value dot_product is guaranteed to
+         * never extrapolate 1. However, there might be rounding errors,
+         * and the value of dot_product may be 1 + (machine epsilon), for instance.
+         * In this case, dot_product > 1 and std::acos is required by the law
+         * (C99 Standard, N1256, section 7.12.4.1) to produce a domain error,
+         * rendering the return value unreliable (7.12.1, paragraph 2).
+         *
+         * Therefore, if dot_product > 1, it is due to rounding errors,
+         * and we may force the angle to be 0.
+         */
+        if( dot_product > 1 )
+            ret.main = 0;
+        else
+            ret.main = std::acos( pp.dot(fp) );
 
         // Fix again if is wrong
         if( cv::norm( pp - rotate_around_axis(_up, fp, -ret.main) )
