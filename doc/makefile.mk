a0ce3e7ae0e0528d495c515ce7ab87a9dc879a62
Tiago Royer <royertiago@gmail.com>
2015-12-09 16:15:29 -0200

    Add doc/makefile.mk, to automagically build the report.

---
+++ doc/makefile.mk
@@ -0,0 +1,24 @@
+docdir := $(dir $(lastword $(MAKEFILE_LIST)))
+
+# Stripped slash directory
+docsdir := $(patsubst %/,%,$(docdir))
+
+pdf := $(docdir)report.pdf
+
+.PHONY: doc
+all: doc
+doc: $(pdf)
+
+$(pdf): %.pdf : %.tex
+	cd $(docdir);\
+	latexmk -pdf $(notdir $<) -pdflatex="pdflatex --shell-escape %O %S"
+
+.PHONY: doc-mostlyclean
+mostlyclean: doc-mostlyclean
+doc-mostlyclean:
+	cd $(docdir) && latexmk -c
+
+.PHONY: doc-clean
+clean: doc-clean
+doc-clean:
+	cd $(docdir) && latexmk -C
