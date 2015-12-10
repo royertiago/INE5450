d953533601dc46577d6ddf13449003965b8015cc
Tiago Royer <royertiago@gmail.com>
2015-12-10 16:13:13 -0200

    math.cpp: Use function safe_acos to avoid NaN's.

--- math.cpp
+++ math.cpp
@@ -2,6 +2,41 @@
 
 /* Based on page 304 of book "Graphics Gems".
  */
+#include <iostream>
+
+/* Returns std::acos(cosine) if cosine is on the interval [-1, 1].
+ * Returns 0 if cosine > 1, and M_PI if cosine < -1.
+ *
+ * Mathematically speaking, there is no angle whose cosine is the given value
+ * if cosine > 1 or cosine < -1.
+ * However, since we are dealing with the real world,
+ * there can be rounding errors that yield values like 1 + 1e10.
+ * In this case, std::acos is required by the law
+ * (C99 Standard, N1256, section 7.12.4.1) to produce a domain error,
+ * rendering the return value unreliable (7.12.1, paragraph 2).
+ * (The GNU implementation returns NaN.)
+ *
+ * Thus, to mitigate this problem and return a minimally meaningful value,
+ * this function assumes values outside [-1, 1] are either -1 or 1.
+ */
+double safe_acos( double cosine ) {
+    /* Mathematically, t
+     * never extrapolate 1. However, there might be rounding errors,
+     * and the value of dot_product may be 1 + (machine epsilon), for instance.
+     * In this case, dot_product > 1 and std::acos is required by the law
+     * (C99 Standard, N1256, section 7.12.4.1) to produce a domain error,
+     * rendering the return value unreliable (7.12.1, paragraph 2).
+     *
+     * Therefore, if dot_product > 1, it is due to rounding errors,
+     * and we may force the angle to be 0.
+     */
+    if( cosine > 1 )
+        return 0;
+    if( cosine < -1 )
+        return M_PI;
+    return std::acos( cosine );
+}
+
 cv::Mat line_line_intersection(cv::Mat p1, cv::Mat p2, cv::Mat q1, cv::Mat q2) {
     cv::Mat pd = p2 - p1; // p delta;
     cv::Mat qd = q2 - q1;
@@ -11,7 +46,7 @@ cv::Mat line_line_intersection(cv::Mat p1, cv::Mat p2, cv::Mat q1, cv::Mat q2) {
 }
 
 double angle_of_projection( double actual_angle, double projected_angle ) {
-    return std::acos( std::sqrt(
+    return safe_acos( std::sqrt(
                 (1 - std::cos(actual_angle)) /
                 (1 - std::cos(projected_angle))
             ));
@@ -27,7 +62,7 @@ cv::Mat compute_rotation_axis(cv::Mat a, cv::Mat b, double projection_angle ) {
     cv::Mat cross = a.cross(b);
     cv::Mat m = (a + b)/2;
 
-    double d_angle = std::acos( std::sin(projection_angle)/cv::norm(m) );
+    double d_angle = safe_acos( std::sin(projection_angle)/cv::norm(m) );
     return rotate_around_axis( m.cross(cross), m, d_angle );
 }
 
@@ -38,19 +73,5 @@ cv::Mat project_on_axis( cv::Mat vector, cv::Mat axis ) {
 
 double angle_between( cv::Mat a, cv::Mat b ) {
     double dot_product = a.dot(b) / (cv::norm(a) * cv::norm(b));
-    /* Mathematically, the value dot_product is guaranteed to
-     * never extrapolate 1. However, there might be rounding errors,
-     * and the value of dot_product may be 1 + (machine epsilon), for instance.
-     * In this case, dot_product > 1 and std::acos is required by the law
-     * (C99 Standard, N1256, section 7.12.4.1) to produce a domain error,
-     * rendering the return value unreliable (7.12.1, paragraph 2).
-     *
-     * Therefore, if dot_product > 1, it is due to rounding errors,
-     * and we may force the angle to be 0.
-     */
-    if( dot_product > 1 )
-        return 0;
-    if( dot_product < -1 )
-        return M_PI;
-    return std::acos(dot_product);
+    return safe_acos( dot_product );
 }
